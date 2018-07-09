04cad8a998f4d74509f33d0d0135b9fa026d516f
Dan Hromada <hromada.dan@gmail.com>
2018-07-09 17:17:20 +0200

    development environment
    Added nodemon
    Added debug messanger tool
    Added .env config file

--- web/index.js
+++ web/index.js
@@ -4,13 +4,22 @@
  *  1. render json + hbs => html
  *  2. serve assets
  */
+require('dotenv').config();
 
 const http = require('http');
 const port = process.env.PORT || 3000;
+const debug = require('debug')('webserver');
 
-//create a server object:
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
-}).listen(port);
+(() => {
+    try {
+        debug('Stand up, webserver');
+        http.createServer((req, res) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write('Hello World!'); //write a response to the client
+            res.end(); //end the response
+        }).listen(port);
+        debug(`Ready and listening on http://localhost:${port}`);
+    } catch (exception) {
+        debug('Exception', exception);
+    }
+})();
