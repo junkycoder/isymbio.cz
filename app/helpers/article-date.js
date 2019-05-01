b6ad1f680b51c0dc5feb5c3d64e58b8202dc61d1
Dan Hromada <hromada.dan@gmail.com>
2019-05-01 09:16:01 +0200

    Add article date (if any) under the title

--- app/helpers/article-date.js
+++ app/helpers/article-date.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 
 module.exports = function(strdate) {
+    if (!strdate) return '';
     const date = new Date(strdate);
     return [date.getDate(), date.getMonth(), date.getFullYear()].join('. ');
 };
