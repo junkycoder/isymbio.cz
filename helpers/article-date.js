6f7b2422c3eb951492c8164a586d33b24e7ccfdd
Dan Hromada <hromada.dan@gmail.com>
2019-05-04 10:11:33 +0200

    Fix news dates

--- helpers/article-date.js
+++ helpers/article-date.js
@@ -3,5 +3,5 @@ const cheerio = require('cheerio');
 module.exports = function(strdate) {
     if (!strdate) return '';
     const date = new Date(strdate);
-    return [date.getDate(), date.getMonth(), date.getFullYear()].join('. ');
+    return [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('. ');
 };
